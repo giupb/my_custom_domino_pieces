0e262e61022024a310de54bf4cc1830de5c32efa
giupb <giulio.pbrugalli@gmail.com>
2023-05-24 13:12:54 -0300

    implement display results

--- pieces/GathererStoriesFromInstagramPostsCaptionPiece/piece.py
+++ pieces/GathererStoriesFromInstagramPostsCaptionPiece/piece.py
@@ -7,6 +7,23 @@ class GathererStoriesFromInstagramPostsCaptionPiece(BasePiece):
         all_captions = ""
         for i in sorted_media_list:
             all_captions += f"Begin of a story\n{i.get('caption')}End of a story\n"
+        
+        self.format_display_result(all_captions)
+        
         return OutputModel(
             all_captions=all_captions
         )
+    
+    def format_display_result(self, all_captions: str):
+        md_text = f"""
+## All Captions
+{all_captions}
+
+"""
+        file_path = f"{self.results_path}/display_result.md"
+        with open(file_path, "w") as f:
+            f.write(md_text)
+        self.display_result = {
+            "file_type": "md",
+            "file_path": file_path
+        }
--- pieces/GetLastVideoUrlYoutubePiece/piece.py
+++ pieces/GetLastVideoUrlYoutubePiece/piece.py
@@ -6,6 +6,23 @@ class GetLastVideoUrlYoutubePiece(BasePiece):
         youtube_videos = input_model.youtube_videos_list
         sorted_youtube_videos = sorted(youtube_videos, key=lambda k: k['publishedAt'])
         last_video_url = sorted_youtube_videos[0]['url']
+
+        self.format_display_result(last_video_url)
+
         return OutputModel(
             last_video_url=last_video_url
         )
+    
+    def format_display_result(self, last_video_url: str):
+        md_text = f"""
+## Last Video URL
+[{last_video_url}]({last_video_url})
+
+"""
+        file_path = f"{self.results_path}/display_result.md"
+        with open(file_path, "w") as f:
+            f.write(md_text)
+        self.display_result = {
+            "file_type": "md",
+            "file_path": file_path
+        }
