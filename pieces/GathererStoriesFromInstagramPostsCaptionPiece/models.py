82fed236074af4077c40dad4136bb956dd2ded48
giupb <giulio.pbrugalli@gmail.com>
2023-05-25 18:55:49 -0300

    new output options for GathererStoriesPiece

--- pieces/GathererStoriesFromInstagramPostsCaptionPiece/models.py
+++ pieces/GathererStoriesFromInstagramPostsCaptionPiece/models.py
@@ -1,4 +1,13 @@
 from pydantic import BaseModel, Field
+from enum import Enum
+
+class OutputTypeType(str, Enum):
+    """
+    Output type for the result text
+    """
+    file = "file"
+    string = "string"
+    file_and_string = "file_and_string"
 
 
 class InputModel(BaseModel):
@@ -9,13 +18,27 @@ class InputModel(BaseModel):
         ...,
         description = "list of Instagram media posts"
     )
+    output_type: OutputTypeType = Field(
+        default=OutputTypeType.string,
+        description='The type of the output.'
+    )
+    output_file_name: str = Field(
+        default="all_stories.txt",
+        description="The name of the file to save all the stories. It works only with Output Type equals to file or file_and_string"
+    )
+
 
 class OutputModel(BaseModel):
     """
     Piece Outputs
     """
-    all_captions: str = Field(
-        description="all captions from Instagram media posts"
+    all_stories_string: str = Field(
+        default=None,
+        description="all captions from Instagram media posts in string format"
+    )
+    all_stories_file_path: str = Field(
+        default=None,
+        description="all captions from Instagram media posts in txt file format"
     )
 
 
--- pieces/GathererStoriesFromInstagramPostsCaptionPiece/piece.py
+++ pieces/GathererStoriesFromInstagramPostsCaptionPiece/piece.py
@@ -4,20 +4,37 @@ from .models import InputModel, OutputModel
 class GathererStoriesFromInstagramPostsCaptionPiece(BasePiece):
     def piece_function(self, input_model: InputModel):
         sorted_media_list = sorted(input_model.instagram_media_list, key=lambda x: x['timestamp'])
-        all_captions = ""
+        all_stories = ""
         for i in sorted_media_list:
-            all_captions += f"Begin of a story\n{i.get('caption')}End of a story\n"
+            all_stories += f"Begin of a story\n{i.get('caption')}End of a story\n"
         
-        self.format_display_result(all_captions)
+        self.format_display_result(all_stories)
+
+        if input_model.output_type == "string":
+            return OutputModel(
+                all_stories_string=all_stories
+            )
         
+        output_file_path = f"{self.results_path}/{input_model.output_file_name}"
+        with open(output_file_path, "w") as f:
+            f.write(all_stories)
+        
+        if input_model.output_type == "file":
+            self.logger.info(f"Result returned as file in {output_file_path}")
+            return OutputModel(
+                all_stories_file_path=output_file_path
+            )
+
+        self.logger.info(f"Result returned as string and file in {output_file_path}")
         return OutputModel(
-            all_captions=all_captions
+            all_stories_string=all_stories,
+            all_stories_file_path=output_file_path,
         )
     
-    def format_display_result(self, all_captions: str):
+    def format_display_result(self, all_stories: str):
         md_text = f"""
-## All Captions
-{all_captions}
+## All Stories  \n
+{all_stories}
 
 """
         file_path = f"{self.results_path}/display_result.md"
